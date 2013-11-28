e0cf179e7f47b9f3b29927eda1d04f9cab1ea75d
huangcd <huangcd.thu@gmail.com>
2013-11-28 09:23:55 +0800

    Solved http://oj.leetcode.com/problems/linked-list-cycle/

---
+++ test/com/leetcode/huangcd/LinkedListCycleTest.java
@@ -0,0 +1,23 @@
+package com.leetcode.huangcd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Date: 11/28/13
+ * <p/>
+ * Time: 9:20 AM
+ * <p/>
+ *
+ * @author chhuang@microsoft.com
+ */
+public class LinkedListCycleTest {
+
+    @Test
+    public void testHasCycle() throws Exception {
+        ListNode head = ListNode.fromArray(1, 2, 3, 4, 5, 6, 7, 8);
+        Assert.assertFalse(new LinkedListCycle().hasCycle(head));
+        head.next.next.next.next.next = head.next.next;
+        Assert.assertTrue(new LinkedListCycle().hasCycle(head));
+    }
+}
