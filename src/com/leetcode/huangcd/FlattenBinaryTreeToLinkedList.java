4b2c5144d86d9f456ee27fd6500edc534ff1918c
huangcd <huangcd.thu@gmail.com>
2013-12-12 09:16:34 +0800

    More problems solved

--- src/com/leetcode/huangcd/FlattenBinaryTreeToLinkedList.java
+++ src/com/leetcode/huangcd/FlattenBinaryTreeToLinkedList.java
@@ -1,7 +1,6 @@
 package com.leetcode.huangcd;
 
 /**
-<<<<<<< HEAD
  * Date: 12/6/13
  * <p/>
  * Time: 5:29 PM
@@ -24,17 +23,5 @@ public class FlattenBinaryTreeToLinkedList {
             root.left = null;
         }
         flatten(root.right);
-=======
- * Date: 12/4/13
- * <p/>
- * Time: 9:05 PM
- * <p/>
- *
- * @author chhuang at live dot cn
- */
-public class FlattenBinaryTreeToLinkedList {
-    public void flatten(TreeNode root) {
-
->>>>>>> 0d5bbb2fffe5e3be09fe84db589f5b8df6286cc8
     }
 }
